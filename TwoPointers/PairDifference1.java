d6ea94a9e6f87fad4aa215596077e4ed266b76d6
Muthu <muthu.rama.krishnan96@gmail.com>
2023-09-10 21:36:48 +0530

    PairDifference1

--- TwoPointers/PairDifference1.java
+++ TwoPointers/PairDifference1.java
@@ -20,25 +20,39 @@ public class PairDifference1 {
 
         int i = 0;
         int j = i + 1;
+        int count = 0;
         boolean hasNum = false;
         while (i <= j && i < n && j < n) {
             int currentDiff = arr[j] - arr[i];
             if (currentDiff < diff) {
                 j++;
                 continue;
-            }
-            else if (currentDiff > diff) {
+            } else if (currentDiff > diff) {
                 i++;
                 continue;
-            }
-            else if (currentDiff == diff && i != j) {
+            } else if (currentDiff == diff && i != j) {
+                int num1 = arr[i];
+                int num1Count = 0;
+                int num2 = arr[j];
+                int num2Count = 0;
+                while (i < n && arr[i] == num1) {
+                    num1Count++;
+                    i++;
+                }
+                while (j < n && arr[j] == num2) {
+                    num2Count++;
+                    j++;
+                }
+                if (num1 == num2) {
+                    count += (num1Count * (num1Count - 1)) / 2;
+                } else {
+                    count += num1Count * num2Count;
+                }
                 hasNum = true;
-                break;
-            }
-            else{
+            } else {
                 j++;
             }
         }
-        System.out.println(hasNum);
+        System.out.println(hasNum ? count : 0);
     }
 }
