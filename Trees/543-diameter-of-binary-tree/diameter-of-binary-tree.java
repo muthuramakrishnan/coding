51afab272b5b7a788eecb89e787d8f3667e26602
muthuramakrishnan <74167303+muthuramakrishnan@users.noreply.github.com>
2023-11-19 16:28:25 +0530

    Time: 2 ms (18.02%) | Memory: 42.9 MB (8.37%) - LeetSync

--- Trees/543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ Trees/543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,28 +14,33 @@
  * }
  */
 class Solution {
-    public int findHeight(TreeNode root){
+    public int findHeight(TreeNode root, Map<TreeNode, Integer> htMap){
         if(root == null){
             return 0;
         }
-        int leftDepth = findHeight(root.left);
-        int rightDepth = findHeight(root.right);
-        return 1 + Math.max(leftDepth, rightDepth);
+        int leftDepth = findHeight(root.left, htMap);
+        int rightDepth = findHeight(root.right, htMap);
+        int height = 1 + Math.max(leftDepth, rightDepth);
+        htMap.put(root, height);
+        return height;
     }
-    public int findDiameter(TreeNode root){
+    public int findDiameter(TreeNode root, Map<TreeNode, Integer> htMap){
         if(root == null){
             return 0;
         }
-        int leftHeight = findHeight(root.left);
-        int rightHeight = findHeight(root.right);
+        int leftHeight = htMap.containsKey(root.left) ? htMap.get(root.left) : findHeight(root.left, htMap);
+        int rightHeight = htMap.containsKey(root.right) ? htMap.get(root.right): findHeight(root.right, htMap);
         int diameter = 1 + leftHeight + rightHeight;
-        int maxOfLeftRight = Math.max(findDiameter(root.left), findDiameter(root.right));
+        int maxOfLeftRight = Math.max(findDiameter(root.left, htMap), findDiameter(root.right, htMap));
         return Math.max(diameter, maxOfLeftRight);
     }
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null){
             return 0;
         }
-        return findDiameter(root) - 1;
+        int leftLevel = 0;
+        int rightLevel = 0;
+        Map<TreeNode, Integer> htMap = new HashMap<>();
+        return findDiameter(root, htMap) - 1;
     }
 }
