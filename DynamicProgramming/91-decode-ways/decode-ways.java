37e0a8b1793d4ad9c4c0480ca81b05468bddc531
muthuramakrishnan <74167303+muthuramakrishnan@users.noreply.github.com>
2024-01-28 19:14:02 +0530

    Time: 1 ms (79.26%) | Memory: 42 MB (15.82%) - LeetSync

--- DynamicProgramming/91-decode-ways/decode-ways.java
+++ DynamicProgramming/91-decode-ways/decode-ways.java
@@ -1,37 +1,61 @@
 class Solution {
     private int[] memoizedValues;
+    private boolean invalidSequence;
 
-    public int countWays(String s, int startIdx) {
-        int n = s.length();
-        if (startIdx >= n) {
+    Solution(){
+        invalidSequence = false;
+    }
+
+    public boolean isValid(int num) {
+        if (num >= 1 && num <= 26) {
+            return true;
+        }
+        return false;
+    }
+
+    public int countDecodings(String s, int startIdx, int n) {
+        if (startIdx < 0) {
             return 1;
         }
-        int s1 = Character.getNumericValue(s.charAt(startIdx));
-        int s2 = startIdx + 2 <= n ? Integer.parseInt(s.substring(startIdx, startIdx + 2)) : -1;
 
-        if (memoizedValues[startIdx] != -1) {
+        if(memoizedValues[startIdx] != -1){
             return memoizedValues[startIdx];
         }
+        
+        int firstNum = Character.getNumericValue(s.charAt(startIdx));
+        int secondNum = startIdx - 1 >= 0 ? Integer.parseInt(s.substring(startIdx - 1, startIdx + 1)) : -1;
+        boolean isDoubleDigitSecondNum = Integer.toString(secondNum).length() == 2;
 
+        // if (!isValid(firstNum)) {
+        //     if(firstNum == 0 && startIdx == n-1){
+        //         //do nothing
+        //     }
+        //     else{
+        //         memoizedValues[startIdx] = 0;
+        //         invalidSequence = true;
+        //         return 0;
+        //     }
+
+        // }
         int way1 = 0;
         int way2 = 0;
-        if(s1 < 1 || s1 > 27){
-            memoizedValues[startIdx] = 0;
-            return 0;
+        if(isValid(firstNum)){
+            way1 = countDecodings(s, startIdx - 1, n);
         }
-        way1 = countWays(s, startIdx + 1);
-        if (s2 >= 1 && s2 <= 26) {
-            way2 = countWays(s, startIdx + 2);
+        if (isValid(secondNum) && isDoubleDigitSecondNum) {
+            way2 = countDecodings(s, startIdx - 2, n);
         }
+
         memoizedValues[startIdx] = way1 + way2;
         return memoizedValues[startIdx];
 
     }
 
     public int numDecodings(String s) {
-        
+        int n = s.length();
         memoizedValues = new int[s.length() + 5];
         Arrays.fill(memoizedValues, -1);
-        return countWays(s, 0);
+        int numDecodings = countDecodings(s, n-1, n);
+        return invalidSequence ? 0 : numDecodings;
     }
 }
