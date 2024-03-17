38d67e8c896d5b9b00b1bbe8347f54af0fa6f501
muthuramakrishnan <74167303+muthuramakrishnan@users.noreply.github.com>
2024-03-17 21:47:22 +0530

    Time: 21 ms (55.16%), Space: 45.4 MB (90.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,4 +1,10 @@
 class Solution {
+    public static void deepCopy(int[] arr1, int[] arr2){
+        for(int i=0; i<arr1.length; i++){
+            arr1[i] = arr2[i];
+        }
+    }
+
     public int longestCommonSubsequence(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
@@ -17,7 +23,8 @@ class Solution {
                     currArr[j] = Math.max(i > 0 ? prevArr[j] : 0, j > 0 ? currArr[j - 1] : 0);
                 }
             }
-            prevArr = currArr;
+            prevArr = new int[n];
+            deepCopy(prevArr, currArr); 
 
         }
         return currArr[n - 1];
